deec4055db1c6b5a96c097644076f6b0d14bc37a
bs3589 <blakesmith3589@gmail.com>
2017-03-21 09:30:33 -0400

    tomogotchi hw

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -1,9 +1,52 @@
 console.log('tamagotchi file is loaded');
 
-//your constructor function here
+
+// constructor
+
+function tamagotchi (name, creatureType) {
+	this.name = name;
+	this.creatureType = creatureType;
+	this.foodInTummy = 10;
+  	this.restedness = 10;
+  	this.health = 10;
+	tamagotchi.cry = function() {
+    	this.foodInTummy -1;
+    	console.log(this.name + "WAHH!!!");
+    };
+
+  	tamagotchi.puke = function() {
+    	this.health -1;
+    };
+
+    tamagotchi.yawn = function(){
+    	this.restedness -1;
+    };
+
+
+
+
+
+};
+
+
 
 
 //create new Tamagotchis
 
+// prototypes
+
+var Frank = new tamagotchi('Frank', 'beast');
+
+var Meg = new tamagotchi('Meg', 'beauty');
+
+
+
+
+
+  
+
 
 //test out your Tamagotchies below via console.logs
+
+
+console.log(Frank.creatureType);
